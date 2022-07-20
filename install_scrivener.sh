116c11486632d056946762a32b33cb09c5738528
Erik Heeren <erik.heeren@hgst.com>
2022-07-20 10:28:04 +0200

    Initial implementation of steps required to start from server, all the way up to scrivener

---
+++ install_scrivener.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+if [ -z "${DISPLAY}" ]
+then
+    echo "Please run this script in a shell that has access to an X session"
+    exit 1
+fi
+
+mkdir -p ~/Downloads
+cd ~/Downloads
+
+echo 'Ping through wine to initialize the environment as 32bit'
+WINEARCH=win32 WINEPREFIX=~/.wine ping google.com
+
+echo 'Winetricks for required software'
+winetricks win10
+winetricks dotnet48
+winetricks speechsdk
+winetricks corefonts
+
+echo 'Download and install scrivener'
+curl -O 'https://scrivener.s3.amazonaws.com/Scrivener-installer-x86.exe'
+wine Scrivener-installer-x86.exe
